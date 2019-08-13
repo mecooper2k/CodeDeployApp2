948958b4b3def06c4da238f6750e3ac845579108
mecooper2k <32883679+mecooper2k@users.noreply.github.com>
2019-08-13 12:38:58 -0700

    Update apache_install.sh

--- scripts/apache_install.sh
+++ scripts/apache_install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 sudo apt-get update -y
-sudo apt-get install -y httpd24 php56 php56-mysqlnd
-sudo service httpd start
-sudo chkconfig httpd on
-sudo groupadd www
-sudo usermod -a -G www ec2-user
-exit
-sudo chgrp www /var/www
-sudo chmod 2775 /var/www
-find /var/www -type d -exec sudo chmod 2775 {} +
-find /var/www -type f -exec sudo chmod 0664 {} +     
+sudo apt-get install apache2 -y  
+sudo service apache2 start
+#sudo chkconfig httpd on
+#sudo groupadd www
+#sudo usermod -a -G www ec2-user
+#exit
+#sudo chgrp www /var/www
+#sudo chmod 2775 /var/www
+#find /var/www -type d -exec sudo chmod 2775 {} +
+#find /var/www -type f -exec sudo chmod 0664 {} +  
